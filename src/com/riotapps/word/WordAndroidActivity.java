13c54d1a5e5ff51405ad1f6573b7842223b2f789
Hunter Eskew <hunter@riotapps.com>
2012-06-25 08:48:01 -0400

    project creation

---
+++ src/com/riotapps/word/WordAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.riotapps.word;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WordAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
