a4fb5690c7a31cbbb501bb71e91b57a7fbc17027
Hunter Eskew <hunter@riotapps.com>
2012-06-30 11:06:26 -0400

    more welcome layout

--- src/com/riotapps/word/Welcome.java
+++ src/com/riotapps/word/Welcome.java
@@ -2,9 +2,11 @@ package com.riotapps.word;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Welcome  extends Activity {
     /** Called when the activity is first created. */
@@ -16,6 +18,20 @@ public class Welcome  extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
+        
+
+        TextView tvWelcome =(TextView)findViewById(R.id.welcome_msg);
+        TextView tvSubWelcome =(TextView)findViewById(R.id.welcome_sub_msg);
+        TextView tvFB =(TextView)findViewById(R.id.byFacebook);
+        TextView tvNative =(TextView)findViewById(R.id.byEmail);
+        Typeface face=Typeface.createFromAsset(getAssets(),"fonts/FullDeceSans1.0.ttf");
+
+        tvWelcome.setTypeface(face);
+        tvSubWelcome.setTypeface(face);
+        tvFB.setTypeface(face);
+        tvNative.setTypeface(face);
+        
+
         
      //   bNative = (Button) findViewById(R.id.btnNative);
      //   bNative.setOnClickListener(new View.OnClickListener() {
