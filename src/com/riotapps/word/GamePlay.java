5325699e395aae0c2f29dd4dcff7442dac53789f
Hunter Eskew <hunter@riotapps.com>
2012-07-27 16:08:43 -0400

    game board, contd

--- src/com/riotapps/word/GamePlay.java
+++ src/com/riotapps/word/GamePlay.java
@@ -2,12 +2,27 @@ package com.riotapps.word;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class GamePlay extends Activity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameplay);
-            }
+        
+    	DisplayMetrics displaymetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+		int vpHeight = displaymetrics.heightPixels;
+		int vpWidth = displaymetrics.widthPixels;
+		
+		TableLayout gameBoardWrapper = (TableLayout) findViewById(R.id.gameBoardWrapper);
+		TableLayout.LayoutParams params=
+				  new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, 300);
+		gameBoardWrapper.setLayoutParams(params);
+		
+      }
 
 }
--- src/gameviews/GameBoard.java
+++ src/gameviews/GameBoard.java
@@ -2,12 +2,16 @@ package gameviews;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.widget.TableLayout;
 
 public class GameBoard extends TableLayout {
 
 	public GameBoard(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		
+		
+	
 	 
 	}
 
